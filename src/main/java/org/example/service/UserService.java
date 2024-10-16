850a8ff9a10f89e59a7a546a58aa8e84c543b27e
Destin <bdsdmiii@yahoo.fr>
2024-10-16 13:27:41 +0200

    Login backend

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -1,10 +1,10 @@
 package org.example.service;
 
-
 import org.springframework.http.ResponseEntity;
+
 import java.util.Map;
 
 public interface UserService {
     ResponseEntity<String> signUp(Map<String, String> requestMap);
+    ResponseEntity<String> login(Map<String, String> requestMap); // Ajout de la méthode login
 }
-
